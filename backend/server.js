cc66e9497dc16209d8004121a6035801a469a178
384-SupawitWongsupa <supawitwongsupa737@gmail.com>
2025-12-10 02:37:15 +0700

    feat: complete lab 2.1 - express + prisma 7 + supabase + quasar integration

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ app.get('/api/demo', (req, res) => {
             detail: 'Git is a distributed version control system that tracks changes in any set of computer files.'
         },
         docker: {
-            detail: 'Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.'
+            detail: 'ศุปวิทย์ วงศ์สุภา 6604101384'
         }
     });
 });
 
 app.listen(port, () => {
-    console.log(`Backend server running at http://localhost:${port}`);
+    console.log(`Backend server running at http://localhost:${3000}`);
 });
